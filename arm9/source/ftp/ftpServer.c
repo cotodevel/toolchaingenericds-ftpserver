87d0b6584f9a53bd4cf6862f44c0a3a22141f64f
coto <programmer.coto.1988@gmail.com>
2018-12-09 01:21:56 -0300

    at least we have bidirectional commands. But clients sockets are discarded instantly and we need to keep a connection stable

--- arm9/source/ftp/ftpServer.c
+++ arm9/source/ftp/ftpServer.c
@@ -54,7 +54,7 @@ struct sockaddr_in server, client;
 struct stat obj;
 int sock1 = -1, sock2 = -1;
 char buf[100], command[5], filename[20];
-int k, size, srv_len, cli_len, c;
+int k, size, srv_len, cli_len = 0, c;
 int filehandle;
 
 
@@ -78,7 +78,6 @@ int do_ftp_server(){
 			
 			//set server
 			memset(&server, 0, sizeof(struct sockaddr_in));
-			memset(&client, 0, sizeof(struct sockaddr_in));
 			
 			srv_len = sizeof(struct sockaddr_in);
 			server.sin_port = htons((int)FTP_PORT);//default listening port
@@ -87,8 +86,12 @@ int do_ftp_server(){
 			//prepare socket (server)
 			sock1 = socket(AF_INET, SOCK_STREAM, 0);
 			
-			//int i=1;
-			//i=ioctl(sock1, FIONBIO,&i); // Server Socket: Accepted connections can't be blocked or the server will fail to detect client IP
+			int enable = 1;
+			if (setsockopt(sock1, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0){	//socket can be respawned ASAP if it's dropped
+				printf("setsockopt(SO_REUSEADDR) failed");
+			}
+			int i=1;
+			i=ioctl(sock1, FIONBIO,&i); // Server Socket: Sender is non blocking
 			
 			if(sock1 == -1){
 				printf("Socket creation failed");
@@ -122,270 +125,276 @@ int do_ftp_server(){
 			   in the listening state.  The original socket sockfd is unaffected by
 			   this call.
 			*/
-			sock2 = accept(sock1,(struct sockaddr*)&client, &cli_len);
 			
 			//int i=1;
 			//i=ioctl(sock2, FIONBIO,&i);
 			
 			//will print once client is connected
-			printf("[Client Connected]");
-			printf("Address: %s ", inet_ntoa(client.sin_addr));
-			printf("Port: %d ", (int) ntohs(client.sin_port));
+			//printf("[Client Connected]");
+			//printf("Address: %s ", inet_ntoa(client.sin_addr));
+			//printf("Port: %d ", (int) ntohs(client.sin_port));
 			
 			setFTPState(FTP_SERVER_CONNECTING);
 		}
 		break;
 	
 		case(FTP_SERVER_CONNECTING):{
-			ftpResponseSender(sock2, 220, "ANY-DOMAIN.COM FTP Service");	//after this, a command should be received in the DS
-			//todo: find out why client is sending garbage data 
-			
 			setFTPState(FTP_SERVER_WORKING);
 		}
 		
 		case(FTP_SERVER_WORKING):{
-			
-			//Actual FTP Service			
-			char buffer[1024];
-			memset(buffer, 0, 1024);
-			
-			int flags = 0;
-			int ret = recv_all(sock2, (char*)&buffer[0], sizeof(buffer), flags);
-			
-			if((ret < 0) && ((ret != EWOULDBLOCK) && (ret != EAGAIN))){
-				close(sock2);
+			memset(&client, 0, sizeof(struct sockaddr_in));
+			cli_len = sizeof(client);
+			sock2 = accept(sock1,(struct sockaddr*)&client, &cli_len);
+			if(sock2 > 0){
 				
-				setFTPState(FTP_SERVER_IDLE);
-				printf("Client disconnected!. Press A to retry.");
-				switch_dswnifi_mode(dswifi_idlemode);
+				//Actual FTP Service			
+				char buffer[1024];
+				memset(buffer, 0, 1024);
 				
-				scanKeys();
-				while(!(keysPressed() & KEY_A)){
-					scanKeys();
-					IRQVBlankWait();
-				}
-				menuShow();
-				return FTP_SERVER_PROC_FAILED;
-			}
-			
-			//test
-			/*
-			printf("CMD: %s ",command);
-			
-			//LOGIN PART
-			if(!strcmp(command, "USER"))
-			{
-				
-			}
-			
-			if(!strcmp(command, "PASS"))
-			{
-				printf("PASS");
-			}
-			
-			if(!strcmp(command, "ls"))
-			{
-				system("ls >temps.txt");
-				i = 0;
-				stat("temps.txt",&obj);
-				size = obj.st_size;
-				send(sock2, &size, sizeof(int),0);
-				filehandle = open("temps.txt", O_RDONLY);
-				//sendfile(sock2,filehandle,NULL,size);
-			}
-			else if(!strcmp(command,"get"))
-			{
-				sscanf(buf, "%s%s", filename, filename);
-				stat(filename, &obj);
-				filehandle = open(filename, O_RDONLY);
-				size = obj.st_size;
-				if(filehandle == -1)
-					size = 0;
-				send(sock2, &size, sizeof(int), 0);
-				if(size){
-					//sendfile(sock2, filehandle, NULL, size);
-				}
-			}
-			else if(!strcmp(command, "put"))
-			{
-				int c = 0, len;
-				char *f;
-				sscanf(buf+strlen(command), "%s", filename);
-				recv(sock2, &size, sizeof(int), 0);
-				i = 1;
-				while(1)
-				{
-					filehandle = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
-					if(filehandle == -1)
-					{
-						sprintf(filename + strlen(filename), "%d", i);
-					}
-					else
-						break;
-				}
-				f = malloc(size);
-				recv(sock2, f, size, 0);
-				c = write(filehandle, f, size);
-				close(filehandle);
-				send(sock2, &c, sizeof(int), 0);
-			}
-			else if(!strcmp(command, "pwd"))
-			{
-				system("pwd>temp.txt");
-				i = 0;
-				FILE*f = fopen("temp.txt","r");
-				while(!feof(f))
-					buf[i++] = fgetc(f);
-				buf[i-1] = '\0';
-				fclose(f);
-				send(sock2, buf, 100, 0);
-			}
-			else if(!strcmp(command, "cd"))
-			{
-				if(chdir(buf+3) == 0)
-					c = 1;
-				else
-					c = 0;
-				send(sock2, &c, sizeof(int), 0);
-			}
-
-
-			else if(!strcmp(command, "bye") || !strcmp(command, "quit"))
-			{
-				printf("FTP server quitting.. ");
-				i = 1;
-				send(sock2, &i, sizeof(int), 0);
-				//exit(0);
-			}
-			
-			*/
-			bool valid = false;
-			
-			//void * memcpy ( void * destination, const void * source, size_t num );
-			memcpy ((u8*)command,(u8*)buffer, 4);
-			
-			//printf("CMD: %s ",command);
-			//while(1==1);
-			
-			if(!strcmp(command, "USER"))
-			{
-				printf("Sent user resp! ");
-				ftp_cmd_USER(sock2, 200, "password ?");
-				valid = true;
-			}
-			
-			if(!strcmp(command, "PASS"))
-			{
-				printf("Sent pass resp! ");
-				ftp_cmd_PASS(sock2, 200, "ok");
-				valid = true;
-			}
-			
-			if(!strcmp(command, "LIST")){
-				valid = true;
-			}
-			
-			if(!strcmp(command, "STOR")){
-				valid = true;
-			}
-			
-			if(!strcmp(command, "RETR")){
-				valid = true;
-			}
-			
-			//default unsupported, accordingly by: https://tools.ietf.org/html/rfc2389
-			if(!strcmp(command, "FEAT")){
-				ftp_cmd_FEAT(sock2, 211, "no-features");
-				valid = true;
-			}
-			
-			//default unsupported, accordingly by: https://cr.yp.to/ftp/syst.html
-			if(!strcmp(command, "SYST")){
-				printf("sent SYST cmd ");
-				ftp_cmd_SYST(sock2, 215, "UNIX Type: L8");
-				valid = true;
-			}
-			
-			//TYPE:PASV by default
-			if(!strcmp(command, "TYPE")){
-				//227
-				//500, 501, 502, 421, 530
-				printf("set PASV response ");
-				ftp_cmd_TYPE(sock2, 227, "Entering Passive Mode");
-				//while(1==1){}	//after this it does nothing. check why
-				valid = true;
-			}
-			
-			
-			//try 3 CMD
-			if(valid==false){
-				memset(command, 0, sizeof(command));
-				//void * memcpy ( void * destination, const void * source, size_t num );
-				memcpy ((u8*)command,(u8*)buffer, 3);
-			}
-			
-			
-			if(!strcmp(command, "CWD")){
-				
-				//first arg: 	socket will be replaced anyway; 
-				//second arg: 	response number will be overriden by CWD´s file/dir availability
-				//third arg: 	another dir you want to access right now
-				ftp_cmd_CWD(0, 0, "");
-				valid = true;
-			}
-			
-			if(!strcmp(command, "PWD")){
-				
-				//Set default PWD
-				sprintf(currentPath,"%s","/");
+				int flags = 0;
+				int ret = recv_all(sock2, (char*)&buffer[0], sizeof(buffer), flags);
 				
 				/*
-				if (getpwd((const char *)currentPath) != NULL){
+				if((ret < 0) && ((ret != EWOULDBLOCK) && (ret != EAGAIN))){
+					close(sock2);
 					
-					memcpy ( currentPath, (u8*) getpwd((const char *)currentPath), sizeof(getpwd((const char *)currentPath)));
+					setFTPState(FTP_SERVER_IDLE);
+					printf("Client disconnected!. Press A to retry.");
+					switch_dswnifi_mode(dswifi_idlemode);
 					
-					//fprintf(stdout, "Current working dir: %s ", cwd);
-					printf("PWD OK: %s ",currentPath);
-				}
-				else{
-					printf("getcwd() error");
+					scanKeys();
+					while(!(keysPressed() & KEY_A)){
+						scanKeys();
+						IRQVBlankWait();
+					}
+					menuShow();
+					return FTP_SERVER_PROC_FAILED;
 				}
 				*/
 				
+				//test
+				/*
+				printf("CMD: %s ",command);
+				
+				//LOGIN PART
+				if(!strcmp(command, "USER"))
+				{
+					
+				}
+				
+				if(!strcmp(command, "PASS"))
+				{
+					printf("PASS");
+				}
+				
+				if(!strcmp(command, "ls"))
+				{
+					system("ls >temps.txt");
+					i = 0;
+					stat("temps.txt",&obj);
+					size = obj.st_size;
+					send(sock2, &size, sizeof(int),0);
+					filehandle = open("temps.txt", O_RDONLY);
+					//sendfile(sock2,filehandle,NULL,size);
+				}
+				else if(!strcmp(command,"get"))
+				{
+					sscanf(buf, "%s%s", filename, filename);
+					stat(filename, &obj);
+					filehandle = open(filename, O_RDONLY);
+					size = obj.st_size;
+					if(filehandle == -1)
+						size = 0;
+					send(sock2, &size, sizeof(int), 0);
+					if(size){
+						//sendfile(sock2, filehandle, NULL, size);
+					}
+				}
+				else if(!strcmp(command, "put"))
+				{
+					int c = 0, len;
+					char *f;
+					sscanf(buf+strlen(command), "%s", filename);
+					recv(sock2, &size, sizeof(int), 0);
+					i = 1;
+					while(1)
+					{
+						filehandle = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
+						if(filehandle == -1)
+						{
+							sprintf(filename + strlen(filename), "%d", i);
+						}
+						else
+							break;
+					}
+					f = malloc(size);
+					recv(sock2, f, size, 0);
+					c = write(filehandle, f, size);
+					close(filehandle);
+					send(sock2, &c, sizeof(int), 0);
+				}
+				else if(!strcmp(command, "pwd"))
+				{
+					system("pwd>temp.txt");
+					i = 0;
+					FILE*f = fopen("temp.txt","r");
+					while(!feof(f))
+						buf[i++] = fgetc(f);
+					buf[i-1] = '\0';
+					fclose(f);
+					send(sock2, buf, 100, 0);
+				}
+				else if(!strcmp(command, "cd"))
+				{
+					if(chdir(buf+3) == 0)
+						c = 1;
+					else
+						c = 0;
+					send(sock2, &c, sizeof(int), 0);
+				}
+
+
+				else if(!strcmp(command, "bye") || !strcmp(command, "quit"))
+				{
+					printf("FTP server quitting.. ");
+					i = 1;
+					send(sock2, &i, sizeof(int), 0);
+					//exit(0);
+				}
+				
+				*/
+				bool valid = false;
+				
 				//void * memcpy ( void * destination, const void * source, size_t num );
-				//memcpy((u8*)tmpStr,cwd,sizeof(cwd));
+				memcpy ((u8*)command,(u8*)buffer, 4);
 				
-				printf("trying dir: %s",currentPath);
+				//printf("CMD: %s ",command);
+				//while(1==1);
 				
-				if (chdir(currentPath)) {
-					printf("PWD ERROR");
+				if(!strcmp(command, "USER"))
+				{
+					printf("Sent user resp! ");
+					ftp_cmd_USER(sock2, 200, "password ?");
+					valid = true;
 				}
-				else{
-					printf("PWD OK");
+				
+				if(!strcmp(command, "PASS"))
+				{
+					printf("Sent pass resp! ");
+					ftp_cmd_PASS(sock2, 200, "ok");
+					valid = true;
 				}
 				
-				//while(1==1){}	//works so far.
+				if(!strcmp(command, "LIST")){
+					valid = true;
+				}
 				
-				ftp_cmd_PWD(sock2, 257, currentPath);
-				valid = true;
-			}
-			
-			if(!strcmp(command, "MKD")){
-				valid = true;
-			}
-			
-			if(!strcmp(command, "RMD")){
-				valid = true;
-			}
-			
-			if(!valid){
-				ftpResponseSender(sock2, 502, "invalid command");
+				if(!strcmp(command, "STOR")){
+					valid = true;
+				}
+				
+				if(!strcmp(command, "RETR")){
+					valid = true;
+				}
+				
+				//default unsupported, accordingly by: https://tools.ietf.org/html/rfc2389
+				if(!strcmp(command, "FEAT")){
+					ftp_cmd_FEAT(sock2, 211, "no-features");
+					valid = true;
+				}
+				
+				//default unsupported, accordingly by: https://cr.yp.to/ftp/syst.html
+				if(!strcmp(command, "SYST")){
+					printf("sent SYST cmd ");
+					ftp_cmd_SYST(sock2, 215, "UNIX Type: L8");
+					valid = true;
+				}
+				
+				//TYPE:PASV by default
+				if(!strcmp(command, "TYPE")){
+					//227
+					//500, 501, 502, 421, 530
+					printf("set PASV response ");
+					ftp_cmd_TYPE(sock2, 227, "Entering Passive Mode");
+					//while(1==1){}	//after this it does nothing. check why
+					valid = true;
+				}
+				
+				
+				//try 3 CMD
+				if(valid==false){
+					memset(command, 0, sizeof(command));
+					//void * memcpy ( void * destination, const void * source, size_t num );
+					memcpy ((u8*)command,(u8*)buffer, 3);
+				}
+				
+				
+				if(!strcmp(command, "CWD")){
+					
+					//first arg: 	socket will be replaced anyway; 
+					//second arg: 	response number will be overriden by CWD´s file/dir availability
+					//third arg: 	another dir you want to access right now
+					ftp_cmd_CWD(0, 0, "");
+					valid = true;
+				}
+				
+				if(!strcmp(command, "PWD")){
+					
+					//Set default PWD
+					sprintf(currentPath,"%s","/");
+					
+					/*
+					if (getpwd((const char *)currentPath) != NULL){
+						
+						memcpy ( currentPath, (u8*) getpwd((const char *)currentPath), sizeof(getpwd((const char *)currentPath)));
+						
+						//fprintf(stdout, "Current working dir: %s ", cwd);
+						printf("PWD OK: %s ",currentPath);
+					}
+					else{
+						printf("getcwd() error");
+					}
+					*/
+					
+					//void * memcpy ( void * destination, const void * source, size_t num );
+					//memcpy((u8*)tmpStr,cwd,sizeof(cwd));
+					
+					printf("trying dir: %s",currentPath);
+					
+					if (chdir(currentPath)) {
+						printf("PWD ERROR");
+					}
+					else{
+						printf("PWD OK");
+					}
+					
+					//while(1==1){}	//works so far.
+					
+					ftp_cmd_PWD(sock2, 257, currentPath);
+					valid = true;
+				}
+				
+				if(!strcmp(command, "MKD")){
+					valid = true;
+				}
+				
+				if(!strcmp(command, "RMD")){
+					valid = true;
+				}
+				
+				if(!valid){
+					ftpResponseSender(sock2, 502, "invalid command");
+				}
+				
 			}
 		}
 		break;
 	}
 	
 	return FTP_SERVER_PROC_RUNNING;
+	
+	//close(sock2); //close ftp server
 }
 
 /*

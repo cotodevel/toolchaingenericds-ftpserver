b6e1f1b869922f000acff06823d1474fdd249d68
Coto <programmer.coto.1988@gmail.com>
2020-03-20 19:44:01 -0300

    Update Binary:
    	- Use only C++ where it's required to

--- common/ipcfifoTGDSUser.c
+++ common/ipcfifoTGDSUser.c
@@ -48,7 +48,7 @@ USA
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
-void HandleFifoNotEmptyWeakRef(uint32 cmd1,uint32 cmd2,uint32 cmd3,uint32 cmd4){
+void HandleFifoNotEmptyWeakRef(uint32 cmd1,uint32 cmd2){
 	
 	switch (cmd1) {
 		//NDS7: 
@@ -67,7 +67,7 @@ void HandleFifoNotEmptyWeakRef(uint32 cmd1,uint32 cmd2,uint32 cmd3,uint32 cmd4){
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
-void HandleFifoEmptyWeakRef(uint32 cmd1,uint32 cmd2,uint32 cmd3,uint32 cmd4){
+void HandleFifoEmptyWeakRef(uint32 cmd1,uint32 cmd2){
 }
 
 //project specific stuff
